a087d4dbf2e91641ffd6133b7b617d246cfaa365
sarvaiyarutik <sarvaiyarutik18@gmail.com>
2026-06-12 12:50:00 +0530

    add code

--- 02_question.js
+++ 02_question.js
@@ -42,3 +42,51 @@ const all = (another)=>{
 };
 
 all(another);
+
+
+
+
+
+
+// example
+
+
+
+const n = [1,2,3,4,5,6,7,8,9];
+
+const functionN =(n)=>{
+
+    let sumN = 0;
+
+    for(let i=0;i<n.length;i++){
+
+        sumN = n[i] + sumN;
+    }
+
+    console.log(sumN);
+}
+
+
+functionN(n);
+
+
+
+
+// for  in method
+
+
+
+const n2 = [10,20,30,40,50,60,70,80,90];
+
+const functionN2 = (n2)=>{
+
+    let sum2 = 0;
+
+    for(let key of n2){
+        sum2+=key;
+    }
+
+    console.log(sum2)
+}
+
+functionN2(n2);
